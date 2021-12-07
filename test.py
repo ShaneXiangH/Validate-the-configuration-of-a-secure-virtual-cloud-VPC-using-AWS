1869330ac7a249c0742540a2c8ef1d3e1145c1d5
JaredEatsBugs <jaredsalvalee@gmail.com>
2021-12-07 13:57:55 -0800

    modify test.py

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ def runcmd(command):
     subprocess.Popen(command, shell = True)
 
 if __name__ == "__main__":
+    runcmd("ssh -i /path/to/your/.pem/file/ ec2-user@IPv4-PUBLIC-DNS-ADDRESS.compute-1.amazonaws.com")
     runcmd("python scanner.py")
